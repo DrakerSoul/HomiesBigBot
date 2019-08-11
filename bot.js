94ff064c8e3c453c91efd217bf07310ff21ef279
DrakerSoul <51520809+DrakerSoul@users.noreply.github.com>
2019-08-11 06:43:14 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,140 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
+})
+  
+client.on('message', message =>{
+  if(message.content == "!Clutch"){
+        message.reply('Here you go, good homie: Here is ClutchTB/Nick https://cdn.discordapp.com/attachments/603463823264841728/603467918981857290/image0.png ');
+      }
+})
+
+client.on('message', message =>{
+  if(message.content == "!Kylie"){
+        message.reply('Here you go, good homie: Here is Kylie/Yuhmeko https://cdn.discordapp.com/attachments/568641653401976842/609865550390099972/image0.png https://cdn.discordapp.com/attachments/609865697882800160/609920058587611136/KYLIE_EYEBROWS.png ');
+      }
+})
+
+client.on('message', message =>{
+  if(message.content == "!Char"){
+        message.reply('Here you go, good homie: Here is Charlizia https://cdn.discordapp.com/attachments/606385933872529408/609861499279638536/charliz.png ');
+      }
+})
+
+client.on('message', message =>{
+  if(message.content == "!Twisted"){
+        message.reply('Here you go, good homie: Here is S_Twisted https://cdn.discordapp.com/attachments/603463823264841728/603468507157626880/unknown.png ');
+      }
+})
+
+client.on('message', message =>{
+  if(message.content == "!Shack"){
+        message.reply('Here you go, good homie: Here is Shack/unresoIved https://i.gyazo.com/thumb/1200/cd75505e931e9607659ba68ad4b56eca-png.jpg ');
+      }
+})
+
+client.on('message', message =>{
+  if(message.content == "!Turtle"){
+        message.reply('Here you go, good homie: Here is iiAteUrTurtle/Laia https://cdn.discordapp.com/attachments/606808897466007564/608007485084663867/90797ea63f238423b17ad067379f0f04.png ');
+      }
+})
+
+client.on('message', message =>{
+  if(message.content == "!O68"){
+        message.reply('Here you go, good homie: Here is O68 https://cdn.discordapp.com/attachments/603463823264841728/603468344418369547/unknown.png ');
+      }
+})
+
+client.on('message', message =>{
+  if(message.content == "!Maria"){
+        message.reply('Here you go, good homie: Here is Iuxias/Maria https://cdn.discordapp.com/attachments/562968908202508298/580533674114809869/image0.png ');
+      }
+})
+
+client.on('message', message =>{
+  if(message.content == "!Controllng"){
+        message.reply('Here you go, good homie: Here is Controllng (Girl Filter) https://cdn.discordapp.com/attachments/603463823264841728/604938174850138141/Snapchat-185134926.png ');
+      }
+})
+
+client.on('message', message =>{
+  if(message.content == "!Mookiez"){
+        message.reply('Here you go, good homie: Here is Mookiez https://media.discordapp.net/attachments/564916365757251618/604546538139680794/image0.png ');
+      }
+})
+
+client.on('message', message =>{
+  if(message.content == "!Hulyhan"){
+        message.reply('Here you go, good homie: Here is Hulyhan https://cdn.discordapp.com/attachments/603438264766038017/605308211997835356/image0.png ');
+      }
+})
+
+client.on('message', message =>{
+  if(message.content == "!clutch"){
+        message.reply('Here you go, good homie: Here is ClutchTB/Nick https://cdn.discordapp.com/attachments/603463823264841728/603467918981857290/image0.png ');
+      }
+})
+
+client.on('message', message =>{
+  if(message.content == "!kylie"){
+        message.reply('Here you go, good homie: Here is Kylie/Yuhmeko https://cdn.discordapp.com/attachments/568641653401976842/609865550390099972/image0.png https://cdn.discordapp.com/attachments/609865697882800160/609920058587611136/KYLIE_EYEBROWS.png ');
+      }
+})
+
+client.on('message', message =>{
+  if(message.content == "!char"){
+        message.reply('Here you go, good homie: Here is Charlizia https://cdn.discordapp.com/attachments/606385933872529408/609861499279638536/charliz.png ');
+      }
+})
+
+client.on('message', message =>{
+  if(message.content == "!twisted"){
+        message.reply('Here you go, good homie: Here is S_Twisted https://cdn.discordapp.com/attachments/603463823264841728/603468507157626880/unknown.png ');
+      }
+})
+
+client.on('message', message =>{
+  if(message.content == "!shack"){
+        message.reply('Here you go, good homie: Here is Shack/unresoIved https://i.gyazo.com/thumb/1200/cd75505e931e9607659ba68ad4b56eca-png.jpg ');
+      }
+})
+
+client.on('message', message =>{
+  if(message.content == "!turtle"){
+        message.reply('Here you go, good homie: Here is iiAteUrTurtle/Laia https://cdn.discordapp.com/attachments/606808897466007564/608007485084663867/90797ea63f238423b17ad067379f0f04.png ');
+      }
+})
+
+client.on('message', message =>{
+  if(message.content == "!o68"){
+        message.reply('Here you go, good homie: Here is O68 https://cdn.discordapp.com/attachments/603463823264841728/603468344418369547/unknown.png ');
+      }
+})
+
+client.on('message', message =>{
+  if(message.content == "!maria"){
+        message.reply('Here you go, good homie: Here is Iuxias/Maria https://cdn.discordapp.com/attachments/562968908202508298/580533674114809869/image0.png ');
+      }
+})
+
+client.on('message', message =>{
+  if(message.content == "!controllng"){
+        message.reply('Here you go, good homie: Here is Controllng (Girl Filter) https://cdn.discordapp.com/attachments/603463823264841728/604938174850138141/Snapchat-185134926.png ');
+      }
+})
+
+client.on('message', message =>{
+  if(message.content == "!mookiez"){
+        message.reply('Here you go, good homie: Here is Mookiez https://media.discordapp.net/attachments/564916365757251618/604546538139680794/image0.png ');
+      }
+})
+
+client.on('message', message =>{
+  if(message.content == "!hulyhan"){
+        message.reply('Here you go, good homie: Here is Hulyhan https://cdn.discordapp.com/attachments/603438264766038017/605308211997835356/image0.png ');
+      }
+})
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
